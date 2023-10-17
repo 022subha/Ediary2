789e38e6323bdcb7d2889136b2fedd2c5ee991d6
Subhajit <022subha@gmail.com>
2023-10-17 15:30:15 +0530

    Verify Email Fixed a Bit

--- client/src/pages/VerifyEmail.jsx
+++ client/src/pages/VerifyEmail.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import OtpInput from "react-otp-input";
 
 export default function VerifyEmail() {
+  const [otp, setOtp] = useState("");
+
   return (
     <div className="pt-20 w-[100%] min-h-[100vh] flex items-center">
       <div className="max-w-[1300px] mx-auto py-8 flex">
@@ -17,7 +20,25 @@ export default function VerifyEmail() {
           </div>
           <div className="h-2/4 flex justify-center">
             <div className="flex gap-x-6 justify-center items-center">
-              <input
+              <OtpInput
+                value={otp}
+                onChange={setOtp}
+                numInputs={4}
+                renderInput={(props) => (
+                  <input
+                    {...props}
+                    style={{
+                      boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                    }}
+                    className="w-[48px] lg:w-[60px] border-0 bg-white rounded-[0.5rem] text-black aspect-square text-center focus:outline-0 text-xl"
+                  />
+                )}
+                containerStyle={{
+                  justifyContent: "space-between",
+                  gap: "0 6px",
+                }}
+              />
+              {/* <input
                 type="text"
                 className="h-[55px] w-[55px] rounded-md text-black text-center focus:outline-0 text-3xl text-semibold font-mono"
               />
@@ -32,7 +53,7 @@ export default function VerifyEmail() {
               <input
                 type="text"
                 className="h-[55px] w-[55px] rounded-md text-black text-center focus:outline-0 text-3xl text-semibold font-mono"
-              />
+              /> */}
             </div>
           </div>
           <div className="h-1/4  text-center">
